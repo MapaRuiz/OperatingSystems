32a1f55b7cf2752c4f5487de23a207f62ce8efa2
Mapa Ruiz <150563352+MapaRuiz@users.noreply.github.com>
2024-11-11 16:38:23 -0500

    Update sistema_comunicacion.c

--- Cap03/Proyecto_Sistema_Noticias/src/sistema_comunicacion.c
+++ Cap03/Proyecto_Sistema_Noticias/src/sistema_comunicacion.c
@@ -1,3 +1,12 @@
+/**************************************************************
+		Pontificia Universidad Javeriana
+	Autor: Daniel Castro, Maria Ruiz, Daniel Gonzalez, Eliana Cepeda
+	Fecha: Noviembre 2024
+	Materia: Sistemas Operativos
+	Tema: Este proyecto implementa un sistema de distribución de noticias usando procesos en C 
+		y comunicación mediante pipes nominales (FIFOs). El sistema permite que un publicador envíe 
+		noticias a un sistema de comunicación, el cual las distribuye a los suscriptores interesados en temas específicos.
+****************************************************************/
 #include <stdio.h>              // Biblioteca estándar para funciones de entrada/salida
 #include <stdlib.h>             // Biblioteca estándar para funciones generales de C
 #include <string.h>             // Biblioteca para manejo de cadenas de caracteres
